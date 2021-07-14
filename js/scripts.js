f27bc91d76e9510d9e43a24579388fd7265485ee
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-07-14 12:36:33 -0300

    some improvement

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 const EASY = [10, 15, 15];
 const MEDIUM = [15, 17, 40];
 const HARD = [25, 36, 170];
+let isStartedGame = false;
+let currentTime = 0;
 let board;
 let difficulty;
-let currentTime = 0;
-let stopWatch; //setInterval
-let isStartedGame = false;
+let stopWatch; //setInterval id
 const height = window.innerHeight
   || document.documentElement.clientHeight
   || document.body.clientHeight;
-  
+
 const states = {
   MARKED: "marked",
   BOMB: "bomb",
@@ -31,7 +31,6 @@ class Camp {
     if (!this.open) {
       this.marked = !this.marked;
     }
-    return this.marked;
   }
 
   hasGoalAchieved() {
@@ -49,14 +48,15 @@ const easyBtn = document.querySelector("#easy-option");
 const mediumBtn = document.querySelector("#medium-option");
 const hardBtn = document.querySelector("#hard-option");
 
-/* reference for display bombs */
+/* reference for display data */
 const bombs_left = document.querySelector("#bombs-left");
 const timerElement = document.querySelector("#timer");
 
 /* setInterval */
 function startTimer() {
-  if (isStartedGame) return console.log("não começou")
+  if (isStartedGame) return
   console.log("startandoNovo")
+  currentTime = 0;
   stopWatch = setInterval(timer, 1000);
   isStartedGame = true;
 }
@@ -80,7 +80,7 @@ function timer() {
   timerElement.innerHTML = (hours < 10 ? `0${hours}:` : hours) + (min < 10 ? `0${min}:` : min) + (sec < 10 ? `0${sec}` : sec)
 }
 
-/* functions */
+/* functions for update HTML */
 function renderField(board) {
   removeChildElements(minefield);
 
@@ -93,38 +93,6 @@ function renderField(board) {
   }
   let template_columns = "grid-template-columns: repeat(" + board[1].length + ",minmax(25px, 45px));";
   minefield.setAttribute("style", template_columns);
-}
-
-function createCamps() {
-  let arr = Array.from(Array(difficulty[0]), () => new Array(difficulty[1]));
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[1].length; j++) {
-      arr[i][j] = new Camp(i, j);
-    }
-  }
-  return arr;
-}
-
-function mineCamps() {
-  let mines = 0;
-  while (mines < difficulty[2]) {
-    let randRow = Math.floor(Math.random() * board.length);
-    let randCol = Math.floor(Math.random() * board[1].length);
-    if (!board[randRow][randCol].bomb) {
-      board[randRow][randCol].bomb = true;
-      mines++;
-    }
-  }
-}
-
-function changeGameMode(difficulty) {
-  init(difficulty);
-}
-
-function removeChildElements(parent) {
-  while (parent.firstChild) {
-    parent.firstChild.remove();
-  }
 }
 
 function changeClass(evento, camp) {
@@ -153,31 +121,73 @@ function changeClass(evento, camp) {
 
 function getClassFromCamp(camp) {
   if (!camp.open) {
-    if (camp.marked) return "marked";
-    return "close";
+    if (camp.marked) return states.MARKED;
+    return states.CLOSE;
   }
   if (camp.open) {
-    if (camp.bomb) return "bomb";
-    return "open";
+    if (camp.bomb) return states.BOMB;
+    return states.OPEN;
   }
-  return "tem algo faltando!!!";
+  console.erro("Erro com as classes")
+  return "";
+}
+
+function UpdateBombsCount() {
+  let tester = board.flat();
+  let marked = tester.filter(x => x.marked)
+  bombs_left.innerText = `${difficulty[2] - marked.length}/${difficulty[2]}`
+}
+
+function removeChildElements(parent) {
+  while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
 }
 
 /* game functions */
+function createCamps() {
+  const field = Array.from(Array(difficulty[0]), () => new Array(difficulty[1]));
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[1].length; j++) {
+      field[i][j] = new Camp(i, j);
+    }
+  }
+  return field;
+}
+
+function mineCamps() {
+  let mines = 0;
+  while (mines < difficulty[2]) {
+    let randRow = Math.floor(Math.random() * board.length);
+    let randCol = Math.floor(Math.random() * board[1].length);
+    if (!board[randRow][randCol].bomb) {
+      board[randRow][randCol].bomb = true;
+      mines++;
+    }
+  }
+}
+
+function changeGameMode(difficulty) {
+  init(difficulty);
+}
+
 function findCampPosition(item) {
   if (item != undefined) {
     let row = parseInt(item.classList?.item(0).substring(1))
     let col = parseInt(item.classList?.item(1).substring(1))
     return board[row][col];
-
   }
-  return console.log("Erro ao encontrar possição do Campo")
+  console.log("Erro ao encontrar posição do Campo");
+  return null
 }
 
-function markCamp(camp) {
-  camp.flag();
-  changeClass(states.MARKED, camp);
-  calcBombsLeft()
+function markCamp(element) {
+  const camp = findCampPosition(element)
+  if (camp != null && camp instanceof Camp) {
+    camp.flag();
+    changeClass(states.MARKED, camp);
+    UpdateBombsCount()
+  }
 }
 
 function openCamp(camp) {
@@ -200,6 +210,12 @@ function openCamp(camp) {
   if (isWin()) {
     alert("Ganhou!")
   }
+}
+
+//game over
+function gameOver(square) {
+  result.innerHTML = 'BOOM! Game Over!'
+  stopTimer()
 }
 
 function abrirtudo() {
@@ -228,16 +244,10 @@ function getAdjCamps(camp) {
   return adjCamps;
 }
 
+//check for win
 function isWin() {
   let tester = [].concat(...board)
   return tester.every(x => x.hasGoalAchieved())
-}
-
-function calcBombsLeft() {
-  let tester = board.flat();
-  let opens = tester.filter(x => x.open)
-  let marked = tester.filter(x => x.marked)
-  bombs_left.innerText = `Minas: ${difficulty[2] - marked.length}`
 }
 
 function init(gameOptions = EASY) {
@@ -245,7 +255,7 @@ function init(gameOptions = EASY) {
   board = createCamps(difficulty);
   mineCamps(difficulty);
   renderField(board);
-  calcBombsLeft(difficulty);
+  UpdateBombsCount(difficulty);
   stopTimer();
 }
 
@@ -256,7 +266,7 @@ hardBtn.addEventListener("click", () => changeGameMode(HARD));
 
 document.body.addEventListener("contextmenu", (e) => {
   e.target?.classList?.contains("item") && e.preventDefault();
-  e.target?.classList?.contains("item") && markCamp(findCampPosition(e.target));
+  e.target?.classList?.contains("item") && markCamp(e.target);
   e.target?.classList?.contains("item") && startTimer();
 });
 
